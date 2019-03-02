717eca297980f8a75639798977aec276c0e79930
davepast <david.pastuszek@hotmail.com>
2019-03-02 22:07:14 +0100

    DÚ commit

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Log In</title>
+    <title>Administration</title>
 </head>
 <body>
 <br>
@@ -17,36 +17,33 @@
 
 
         <div class="col-md-4">
-            <form method="post">
 
-                <div class="form-group">
-                    <p>Login:</p>
-                    <input type="text" name="login" placeholder="Zadejte login" class="form-control">
-                </div>
-
-                <div class="form-group">
-                <p>Heslo:</p>
-                <input type="password" name="password" placeholder="Zadejte heslo" class="form-control">
-                </div>
-
-                <input type="submit" value="Přihlásit" class="btn btn-primary" >
-            </form>
             <?php
-            session_start();
-    echo "<br>";
-            if (isset($_POST['login'])) {
-                if ($_POST['login'] == "admin" && $_POST['password'] == "top-secret") {
-                    $_SESSION['login'] = $_POST['login'];
-                    $_SESSION['data'] = [
-                        'Datum registrace' => '2.4.2018',
-                        'Souhlas s podmínkami' => 'ANO',
-                        'Počet objednávek' => 23
-                    ];
-                    echo '<a href="admin.php">Pokračovat do administrace</a>';
+session_start();
+                if (isset($_SESSION['login'])) {
+                    echo "Vítej, " . $_SESSION['login'] . "<br>";
+                    echo '<br>'; ?>
+                        <table class="table">
+
+
+
+                            <tbody>
+                            <?php
+                        foreach ($_SESSION['data'] as $key => $value) { ?>
+
+                                <tr>
+                                    <th scope="row"><?php echo $key . ":"; ?></th>
+                                    <td><?php echo $value; ?></td>
+                                </tr>
+                        <?php } ?>
+
+                    </tbody>
+                        </table>
+                    <a href="logout.php">Odhlásit</a>
+            <?php
                 } else {
-                    echo "Chybné jméno nebo heslo";
+                    echo "Přístup zamítnut";
                 }
-            }
             ?>
         </div>
 
--- logout.php
+++ logout.php
@@ -8,46 +8,28 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Log In</title>
+    <title>Log Out</title>
 </head>
 <body>
 <br>
 <div class="container-fluid" >
     <div class="row justify-content-lg-center">
 
-
         <div class="col-md-4">
-            <form method="post">
-
-                <div class="form-group">
-                    <p>Login:</p>
-                    <input type="text" name="login" placeholder="Zadejte login" class="form-control">
-                </div>
-
-                <div class="form-group">
-                <p>Heslo:</p>
-                <input type="password" name="password" placeholder="Zadejte heslo" class="form-control">
-                </div>
-
-                <input type="submit" value="Přihlásit" class="btn btn-primary" >
-            </form>
             <?php
             session_start();
-    echo "<br>";
-            if (isset($_POST['login'])) {
-                if ($_POST['login'] == "admin" && $_POST['password'] == "top-secret") {
-                    $_SESSION['login'] = $_POST['login'];
-                    $_SESSION['data'] = [
-                        'Datum registrace' => '2.4.2018',
-                        'Souhlas s podmínkami' => 'ANO',
-                        'Počet objednávek' => 23
-                    ];
-                    echo '<a href="admin.php">Pokračovat do administrace</a>';
-                } else {
-                    echo "Chybné jméno nebo heslo";
-                }
+            if (isset($_SESSION['login']) && isset($_SESSION['data'])) {
+                unset($_SESSION['login']);
+                unset($_SESSION['data']);
+                echo "Odhlášení proběhlo úspěšně";
+                echo "<br>";
+                echo '<a href="login.php">Zpět na přihlašovací formulář</a>';
+
+            } else {
+                echo "Nejsi přihlášen";
             }
             ?>
+
         </div>
 
 
